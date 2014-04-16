8eeb84ebb06a72f8a71e01ff67b59a7b62a48ccc
Renato Cassino <renatocassino@gmail.com>
2014-04-15 23:22:16 -0300

    Yii2Hinclude

--- src/TokenPost/Yii2Hinclude.php
+++ src/TokenPost/Yii2Hinclude.php
@@ -4,23 +4,27 @@ namespace TokenPost;
 
 use yii\base\Widget;
 use yii\helpers\Html;
+use yii\helpers\Url;
 use Yii;
 
+/*
+	Yii2Hinclude::begin(['url'=>['/url']]);
+    	echo 'Loaging.....';
+    Yii2Hinclude::end();
+ */
 class Yii2Hinclude extends Widget
 {
-	public $sourcePath = '@vendor/tokenpost/yii2hinclude/src/Assets';
-
-	public $js = [
-		'hinclude.js',
-	];
+	public $url;
 
 	public function init()
 	{
 		Yii2HincludeAsset::register($this->getView());
+
+		echo '<hx:include src="' . Url::toRoute($this->url) . '">';
 	}
 
 	public function run()
 	{
-		
+		echo '</hx>';
 	}
 }

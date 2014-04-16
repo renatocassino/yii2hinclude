bbc00ca8175945a6f8f79d5ba0bcec3479dc58df
Renato Cassino <renatocassino@gmail.com>
2014-04-15 21:48:08 -0300

    Readme file

---
+++ README.md
@@ -0,0 +1,4 @@
+Yii2Hinclude
+===========================
+
+This is a plugin to use Hinclude in Yii2.
--- src/TokenPost/Yii2Hinclude.php
+++ src/TokenPost/Yii2Hinclude.php
@@ -3,6 +3,7 @@
 namespace TokenPost;
 
 use yii\base\Widget;
+use yii\helpers\Html;
 use Yii;
 
 class Yii2Hinclude extends Widget
@@ -12,4 +13,14 @@ class Yii2Hinclude extends Widget
 	public $js = [
 		'hinclude.js',
 	];
+
+	public function init()
+	{
+		Yii2HincludeAsset::register($this->getView());
+	}
+
+	public function run()
+	{
+		
+	}
 }

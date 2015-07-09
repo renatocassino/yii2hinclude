b7031d39000515c54a76ebfb81abd8e8f327acde
Renato <renatocassino@gmail.com>
2015-07-08 23:38:29 -0300

    Tokenpost to tacnoman

--- src/Tacnoman/Yii2HincludeAsset.php
+++ src/Tacnoman/Yii2HincludeAsset.php
@@ -6,7 +6,7 @@ use yii\web\AssetBundle;
 
 class Yii2HincludeAsset extends AssetBundle
 {
-	public $sourcePath = '@vendor/tokenpost/yii2hinclude/src/Assets';
+	public $sourcePath = '@vendor/tacnoman/yii2hinclude/src/Assets';
 
 	public $js = [
 		'hinclude.js',
